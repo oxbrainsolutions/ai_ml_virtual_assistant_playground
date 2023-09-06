8ae9d67635f70b0f89114d3c1c6e5f74f93cdabc
oxbrAIn <135913499+oxbrainsolutions@users.noreply.github.com>
2023-09-06 13:30:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -872,7 +872,7 @@ footer = """
             padding: 0.8em;  /* Adjust padding for smaller screens */
        }
         .left-column-footer {
-            width: 40%;
+            width: 100%;
             justify-content: justify;
             display: flex;
             font-size: 2.2em;
